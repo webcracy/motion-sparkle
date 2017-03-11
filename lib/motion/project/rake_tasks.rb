69638eae309fc1db9f0bcb750eb8910138107c38
Brett Walker <brett@digitalmoksha.com>
2017-03-11 12:26:40 +0100

    don't need to generate the singular appcast when using generate_appcast

--- lib/motion/project/rake_tasks.rb
+++ lib/motion/project/rake_tasks.rb
@@ -53,7 +53,6 @@ namespace :sparkle do
   task :generate_appcast do
     App.config_without_setup.build_mode = :release
     sparkle = App.config.sparkle
-    sparkle.create_appcast
     results = `#{sparkle.sparkle_vendor_path}/generate_appcast "#{sparkle.private_key_path}" "#{sparkle.archive_folder}"`
   end
 
